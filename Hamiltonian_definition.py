11d22c6d1482baa11b87d483ff438c57c1d985df
Cool-stanza <costanzalincetto@gmail.com>
2025-03-18 11:15:38 +0100

    Shastry Sutherland codes

---
+++ Hamiltonian_definition.py
@@ -0,0 +1,193 @@
+import numpy as np
+from scipy.sparse import lil_matrix, csr_matrix
+from scipy.sparse import dok_matrix
+from scipy.sparse import block_diag
+from scipy.sparse.linalg import eigsh
+import scipy.linalg as la
+
+
+# NEAREST NEIGHBORS
+neighbors = []
+for idx, (i, j) in enumerate(coor):
+    neighbor_list = []
+    
+    right = (i+1)%Lx  # if i==Lx-1 nn==0 
+    neighbor_list.append((right, j))  
+    
+    top = (j-1)%Ly  # if j==0, nn==Ly-1 (==3)
+    neighbor_list.append((i, top))  
+    
+    neighbors.append(neighbor_list)
+    
+#---------------------------------------------------
+neighbors_indices = []
+for neighbor_list in neighbors:
+    indices = [coor.index(v) for v in neighbor_list]  # find the index of each nn
+    neighbors_indices.append(indices) 
+#-------------------------------------------------------
+
+"""
+plt.figure(figsize=(3,3))
+plt.scatter(x, y)
+for idx, point in enumerate(coor):
+    plt.text(point[0] + 0.05, point[1] + 0.05, f"{idx}", fontsize=10)
+
+for i, neighbor_list in enumerate(neighbors_indices):
+    for neighbor_idx in neighbor_list:
+        x_vals = [coor[i][0], coor[neighbor_idx][0]]
+        y_vals = [coor[i][1], coor[neighbor_idx][1]]
+        plt.plot(x_vals, y_vals, 'k-', alpha=0.5)  # nn line
+plt.show()
+"""
+
+# NEXT NEIGHBORS - DIAGONALS
+avoid = []
+diagonals = [None]*Lx*Ly
+for idx, (i, j) in enumerate(coor):   
+    if i%2==0 and j%2==0:
+        next_x = (i+1)  
+        next_y = (j+1)
+        diagonals[idx]=((next_x, next_y))
+        avoid.append((i,j))
+        
+    if i%2!=0 and j%2!=0:     
+        next_x = (i-1)  
+        next_y = (j-1)
+        diagonals[idx]=((next_x, next_y))
+        avoid.append((i,j))  
+
+for idx, (i, j) in enumerate(coor):  
+    if (i,j) in avoid:
+        continue
+    elif i%2==0:
+        next_x = (i-1)%Lx 
+        next_y = (j+1)%Ly 
+        #print(i,j, "-->",next_x, next_y )
+        diagonals[idx]=((next_x,next_y))
+    elif i%2!=0:
+        next_x = (i+1)%Lx 
+        next_y = (j-1)%Ly 
+        #print(i,j, "-->",next_x, next_y )
+        diagonals[idx]=((next_x,next_y))
+#print("coor dei secondi vicini",diagonals)
+
+#---------------------------------------------
+diag_indices = []
+for diag_point in diagonals:
+    if diag_point is not None:  #
+        index = coor.index(diag_point)  # Trova l'indice della tupla in coor
+        diag_indices.append([index])  # Crea una lista con l'indice trovato
+    else:
+        diag_indices.append([])  
+#print("Indici dei secondi vicini diagonali:", diag_indices)
+
+#-----------------------------------------------
+
+# next neighbor lattice
+plt.figure(figsize=(3,3))
+plt.scatter(x, y)
+for idx, point in enumerate(coor):
+    plt.text(point[0] + 0.05, point[1] + 0.05, f"{idx}", fontsize=10)
+
+for i, diag_list in enumerate(diag_indices):
+    for diag_idx in diag_list:
+        x_vals = [coor[i][0], coor[diag_idx][0]]
+        y_vals = [coor[i][1], coor[diag_idx][1]]
+        plt.plot(x_vals, y_vals, 'k--', alpha=0.5)  # nn line
+        
+for i, neighbor_list in enumerate(neighbors_indices):
+    for neighbor_idx in neighbor_list:
+        x_vals = [coor[i][0], coor[neighbor_idx][0]]
+        y_vals = [coor[i][1], coor[neighbor_idx][1]]
+        plt.plot(x_vals, y_vals, 'k-', alpha=0.5)  # nn line
+plt.show()
+
+#check
+for idx, (i,j) in enumerate(coor):
+    print(f"{idx} --> Nearest neighbors: {neighbors_indices[idx]}", f" -- Next neighbor: {diag_indices[idx]}")
+
+#-------------------------------------------------------------------------------------------------------------------------
+
+# HAMILTONIAN
+def flip(state,i,j): #flippa lo spin degli indici i e j
+    return state ^ (2**i + 2**j) # ^==xor
+
+def Hamiltonian(J1, J2, state, Lx, Ly):
+    result = []
+    seen_states_1 = set()  # Per evitare duplicati
+    seen_states_2 = set() 
+    seen_states_d = set() 
+    
+    coeff_nn = 0
+    coeff_nnn = 0
+    coeff_field = 0
+    for i in range(Lx*Ly):
+        n = (state & 2**i)/2**i #extract bit at position i --> 0 or 1 --> poi (2n-1)/2 per avere -1/2, +1/2
+        
+        nn_x = neighbors_indices[i][0] #nn dx
+        nn_y = neighbors_indices[i][1] #nn up
+        nn_d = diag_indices[i][0] #nnn
+
+        nn_1 = (state & 2**nn_x)/2**nn_x
+        nn_2 = (state & 2**nn_y)/2**nn_y
+        coeff_nn += (2*n-1)*(2*nn_1-1)/4 + (2*n-1)*(2*nn_2-1)/4 # sarebbe (2*n-1)/2 * (2*nn-1)/2 
+        if n!=nn_1 : 
+            new_state_1 = flip(state,i,nn_x)
+            if new_state_1 not in seen_states_1:
+                result.append([J1/2,new_state_1])
+                seen_states_1.add(new_state_1)
+        if n!=nn_2 : 
+            new_state_2 = flip(state,i,nn_y)
+            #if new_state_2 not in seen_states_2:
+            result.append([J1/2,new_state_2])
+             #   seen_states_2.add(new_state_2)
+        
+        nnn = (state & 2**nn_d)/2**nn_d
+        coeff_nnn += (2*n-1)*(2*nnn-1)/8 #1/2 in più perchè i vicini in diagonale li sto contando due volte
+        if n!=nnn : 
+            new_state_d = flip(state,i,nn_d)
+            if new_state_d not in seen_states_d:
+                result.append([J2/2,new_state_d])
+                seen_states_d.add(new_state_d)
+          
+    result.append([J1*coeff_nn+J2*coeff_nnn, state])
+    return result 
+
+
+#MATRICE DIAGONALE A BLOCCHI
+def build_basisN(L,N): #L=Lx*Ly number of sites, N number of spin up in the sector
+    basisN = []
+    for n in range(2**L):
+        particle_count = bin(n).count('1') #count the 1 in n
+        if particle_count == N:
+            basisN.append(n) #check if n belongs to the N sector
+    return basisN
+
+
+def build_HN(Lx,Ly,N,J1,J2):
+    L=Lx*Ly
+    basisN = build_basisN(L,N)
+    dimN = len(basisN) #dimension of the subspace S_N
+    HN = dok_matrix((dimN,dimN)) #crea matrice vuota sparsa
+
+    for b,n in enumerate(basisN): #b index, n binary state
+        output = Hamiltonian(J1,J2,n,Lx,Ly) #H|n>
+        for coeff,m in output:
+            try:
+                a = basisN.index(m)
+                HN[a,b]+=coeff
+            except ValueError:
+                continue
+    return HN.tocsr() #csr
+
+
+def H_diag_block(Lx,Ly,J1,J2):
+    L=Lx*Ly
+    blocks = []
+    for N in range(L+1):
+        basisN = build_basisN(L,N)
+        if len(basisN) > 0:
+            HN = build_HN(Lx,Ly,N,J1,J2)
+            blocks.append(HN)
+    H = block_diag(blocks, format="csr")
+    return H
